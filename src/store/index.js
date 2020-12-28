5fe8443c0d14f817b54f1997e93c73d7033ac3c1
Clinton Botha <clinton@tangentsolutions.co.za>
2020-12-28 21:26:45 +0200

    code: update intial store definition

--- src/store/index.js
+++ src/store/index.js
@@ -5,25 +5,32 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    is_report_data_loaded: false
+    is_data_loaded: false,
+    is_data_processing: false,
+    has_data_load_error: false
+  },
+  getters: {
+    get_data_load_state: state => {
+      return state.is_data_loaded;
+    }
   },
   mutations: {
-    SET_ORGANISATION_NAME(state, val) {
-      state.organisation_name = val;
+    SET_DATA_LOAD_STATE(state, val) {
+      state.is_data_loaded = val;
     }
   },
   actions: {
-    GET_REPORT_META_DATA({ commit, state }) {
-      if (state.is_report_data_loaded) return;
+    GET_DATA({ commit, state }) {
+      if (state.is_data_loaded) return;
       //
-      state.is_report_data_processing = true;
+      state.is_data_processing = true;
       let url = `/reportapi/`;
       //
       return new Promise((resolve, reject) => {
         this.$http
           .get(url)
           .then(response => {
-            commit("SET_ORGANISATION_NAME", response.data["organisation_name"]);
+            commit("SET_DATA_LOAD_STATE", true);
             commit(
               "SET_USER_CAN_NAVIGATE_REPORT",
               response.data["user_can_navigate_report"]
@@ -38,14 +45,14 @@ export default new Vuex.Store({
             response.data["indicators"].forEach(indicator => {
               commit("SET_INDICATOR", indicator);
             });
-            state.is_report_data_loaded = true;
-            state.is_report_data_processing = false;
-            state.has_report_data_load_error = false;
+            state.is_data_loaded = true;
+            state.is_data_processing = false;
+            state.has_data_load_error = false;
             resolve();
           })
           .catch(error => {
-            state.is_report_data_processing = false;
-            state.has_report_data_load_error = true;
+            state.is_data_processing = false;
+            state.has_data_load_error = true;
             reject(error);
           });
       });
