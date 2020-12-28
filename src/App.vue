6a290fbb183910aa47f886eadc00cad5f61331b2
Clinton Botha <clinton@tangentsolutions.co.za>
2020-12-28 15:19:44 +0200

    code: add app name and subtitle to menu

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title">
-            UX Manual
+            The UX Manual
           </v-list-item-title>
           <v-list-item-subtitle>
-            A reference guide
+            A reference manual
           </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
@@ -26,38 +26,21 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-          @click="drawer = !drawer"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar app color="primary" dark elevation="0">
+      <v-icon @click="drawer = !drawer">
+        mdi-menu
+      </v-icon>
 
       <v-spacer></v-spacer>
 
-      <v-btn
+      <!-- <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
 
     <v-main>
