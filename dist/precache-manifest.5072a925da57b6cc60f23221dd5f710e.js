54080dd86d7fb67491e387bb7d88362b6973992d
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-05 21:32:16 +0200

    build dist

---
+++ dist/precache-manifest.5072a925da57b6cc60f23221dd5f710e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f904c1884c1eeed0d4f",
+    "url": "/css/app.845a1d84.css"
+  },
+  {
+    "revision": "3b1539a7b52c52e5891f",
+    "url": "/css/chunk-vendors.6f8f14db.css"
+  },
+  {
+    "revision": "61a6c402e93855e7d5d463549536a1bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecb5d94b98f2d701f090",
+    "url": "/js/about.3370379f.js"
+  },
+  {
+    "revision": "4f904c1884c1eeed0d4f",
+    "url": "/js/app.bfd926c7.js"
+  },
+  {
+    "revision": "3b1539a7b52c52e5891f",
+    "url": "/js/chunk-vendors.6ea26951.js"
+  },
+  {
+    "revision": "531a29d986cc1ab99b7700233cfa8078",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4a2e052a0263f3a3d9de3280c3b2f201",
+    "url": "/manifest.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "25c6b55a51f2ba3fb04b232b725f8b6a",
+    "url": "/ux_processes.json"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,3 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  pwa: {
-    name: "The UX Manual",
-    themeColor: "#3f51b5",
-    msTileColor: "#000000",
-    appleMobileWebAppCapable: "no",
-    appleMobileWebAppStatusBarStyle: "black",
-
-    // configure the workbox plugin
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: "dev/sw.js"
-      // ...other Workbox options...
-    }
-  }
+  transpileDependencies: ["vuetify"]
 };
