66a52d37564b78308be0f908895af04ef2b231f3
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-05 22:53:48 +0200

    update service worker reference

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "My App",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "service-worker.js"
+      // ...other Workbox options...
+    }
+  }
 };
