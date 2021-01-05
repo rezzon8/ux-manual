9f754003de751123e97bd837a3aec7782af39795
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-05 21:19:11 +0200

    add pwa abilities

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "The UX Manual",
+    themeColor: "#3f51b5",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "no",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js"
+      // ...other Workbox options...
+    }
+  }
 };
