d7c69db8d8f984b4c2fd07e8e54b7308adeea9a9
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-05 22:59:20 +0200

    build dist

---
+++ dist/precache-manifest.6505a9dd8811fceac6e5a47ce48cf097.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f904c1884c1eeed0d4f",
+    "url": "/css/app.845a1d84.css"
+  },
+  {
+    "revision": "3b1539a7b52c52e5891f",
+    "url": "/css/chunk-vendors.6f8f14db.css"
+  },
+  {
+    "revision": "01347ba86e03c2643354f55bd8e647bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecb5d94b98f2d701f090",
+    "url": "/js/about.3370379f.js"
+  },
+  {
+    "revision": "4f904c1884c1eeed0d4f",
+    "url": "/js/app.bfd926c7.js"
+  },
+  {
+    "revision": "3b1539a7b52c52e5891f",
+    "url": "/js/chunk-vendors.6ea26951.js"
+  },
+  {
+    "revision": "f26e25c7a0cf9972a2a2e4ed9a65da92",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "25c6b55a51f2ba3fb04b232b725f8b6a",
+    "url": "/ux_processes.json"
+  }
+]);
