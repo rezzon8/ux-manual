5686c9eba63489532a3cb4e538526e6d8c76c65b
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-06 22:26:07 +0200

    code: add buttons for authentication dialogs

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,20 @@
       <v-toolbar-title class="ml-3">
         <h2>The UX Manual</h2>
       </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>
+        Login
+      </v-btn>
+
+      <v-btn text>
+        Signup
+      </v-btn>
+
+      <v-btn text>
+        Logout
+      </v-btn>
     </v-app-bar>
 
     <v-main>
