4de74f364a4013a8b663708a3f6fb2bbba33bc53
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-07 18:38:35 +0200

    initialize firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import axios from "axios";
-import App from "./App.vue";
+import firebase from "firebase/app";
+import "firebase/auth";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "./registerServiceWorker";
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
@@ -15,5 +17,16 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp = {
+      apiKey: "AIzaSyA_yaIVWnWk8XNTmMW_pTc2OdEKzVvB30w",
+      authDomain: "the-ux-manual.firebaseapp.com",
+      projectId: "the-ux-manual",
+      storageBucket: "the-ux-manual.appspot.com",
+      messagingSenderId: "34229624456",
+      appId: "1:34229624456:web:b03bf6c4f087db6f88c93d",
+      measurementId: "G-5GRTYYWWE5"
+    };
+  }
 }).$mount("#app");
