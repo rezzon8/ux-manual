50a5ee2daa4a3695132e6449978948b2e3298917
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-12 17:40:36 +0200

    refactor test suite

--- tests/unit/components/forms/signup_form.spec.js
+++ tests/unit/components/forms/signup_form.spec.js
@@ -1,5 +1,6 @@
 import { createLocalVue, mount } from "@vue/test-utils";
 import Vue from "vue";
+import Vuex from "vuex";
 import Vuetify from "vuetify";
 import SignUpForm from "@/components/forms/SignUpForm.vue";
 
@@ -9,16 +10,49 @@ const localVue = createLocalVue();
 const vuetify = new Vuetify();
 
 let wrapper;
-// added to prevent Vuetify error
-document.body.setAttribute("data-app", true);
+let store;
+let actions;
+let mutations;
+let state;
 
-describe("UXProcessItem.vue", () => {
-  beforeEach(() => {
-    wrapper = mount(SignUpForm, {
-      localVue,
-      vuetify
-    });
+localVue.use(Vuex);
+
+const factory = (computed = {}) => {
+  return mount(SignUpForm, {
+    propsData: {},
+    mocks: {},
+    stubs: {},
+    methods: {},
+    store,
+    vuetify,
+    computed,
+    localVue
   });
+};
+
+beforeEach(() => {
+  actions = {
+    someAction: jest.fn()
+  };
+  mutations = {
+    someMutation: jest.fn()
+  };
+  state = {
+    key: {}
+  };
+  store = new Vuex.Store({
+    actions,
+    mutations,
+    state
+  });
+  wrapper = factory();
+});
+
+afterEach(() => {
+  wrapper.destroy();
+});
+
+describe("SignUpForm.vue", () => {
   it("should render", () => {
     console.log(wrapper.html());
     expect(wrapper.exists()).toBe(true);
