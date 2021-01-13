ad03ce8002cc512c81425c39c0ec028b96b3b4ab
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-13 16:43:24 +0200

    add spec for App.vue

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -2,80 +2,134 @@ import { createLocalVue, mount } from "@vue/test-utils";
 import Vue from "vue";
 import Vuex from "vuex";
 import Vuetify from "vuetify";
-import UX_manual from "@/App.vue";
+import VueRouter from "vue-router";
+import Vuelidate from "vuelidate";
+import App from "@/App.vue";
 
-Vue.use(Vuetify); // added to prevent Vuetify lint errors
+Vue.use(Vuelidate);
+Vue.use(Vuetify); // added to prevent vuetify lint errors
 
 const localVue = createLocalVue();
 const vuetify = new Vuetify();
 
+let wrapper;
+let router;
+let store;
+let state;
+let getters;
+let actions;
+let mutations;
+
+localVue.use(VueRouter);
 localVue.use(Vuex);
 
-let wrapper;
-// added to prevent Vuetify error
-document.body.setAttribute("data-app", true);
+router = new VueRouter();
 
-const state = {
-  ux_processes: [
-    {
-      id: 1,
-      title: "UX Audit",
-      copy:
-        "After a kickoff meeting about your goals and mission, we conduct a UX review of your current status. As a result, you get an analysis stating the opportunities to develop. We offer UX Audit as an independent service as well.",
-      color: "#3f51b5"
-    }
-  ],
-  process_steps: [
-    {
-      id: 1,
-      title: "Stakeholder Interview",
-      copy: {
-        introduction: [
-          "We kick-off each project with a meeting with you: we need to talk about your mission, goals and context. This meeting ensures that we start the UX design process by understanding the problem. We also need to have a good knowledge of your domain in order to ask the right questions, find the best solutions."
-        ]
-      }
-    }
-  ],
-  dialog: true
+const factory = () => {
+  return mount(App, {
+    propsData: {},
+    data: () => ({
+      email: "test@test.com",
+      password: "12345678",
+      confirmPassword: "12345678",
+      showPassword: false
+    }),
+    mocks: {},
+    stubs: {},
+    store,
+    vuetify,
+    router,
+    localVue
+  });
 };
 
-const actions = {
-  GET_DATA: jest.fn()
-};
-
-const store = new Vuex.Store({ actions, state });
-
-const $route = { path: "/" };
-
-describe("Home.vue", () => {
+describe("App.vue", () => {
   beforeEach(() => {
-    wrapper = mount(UX_manual, {
-      mocks: {
-        $route
-      },
-      store,
-      localVue,
-      vuetify
+    state = {
+      key: {}
+    };
+    getters = {
+      userIsAuthenticated: jest.fn(false)
+    };
+    actions = {
+      GET_DATA: jest.fn(),
+      LOGIN: jest.fn(),
+      SIGN_UP: jest.fn(),
+      LOGOUT: jest.fn()
+    };
+    mutations = {
+      someMutation: jest.fn()
+    };
+    store = new Vuex.Store({
+      state,
+      getters,
+      actions,
+      mutations
     });
+    wrapper = factory();
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
   });
   it("should render", () => {
     expect(wrapper.exists()).toBe(true);
+    expect(wrapper).toMatchSnapshot();
   });
-  it("display a title", () => {
-    console.log(wrapper.html());
-    expect(wrapper.find("h2").html()).toContain("The UX Manual");
-    expect(wrapper.find("h2").exists()).toBe(true);
+  it("should define a signUp method", () => {
+    expect(wrapper.vm.signUp).toBeDefined();
   });
-  it("should have / route", () => {
-    expect(wrapper.vm.$route.path).toBe($route.path);
+  it("should define a logIn method", () => {
+    expect(wrapper.vm.logIn).toBeDefined();
   });
-  it("should contain a dialog", () => {
-    expect(wrapper.get(".v-dialog__container").exists()).toBe(true);
+  it("should define a logOut method", () => {
+    expect(wrapper.vm.logOut).toBeDefined();
   });
-  // it("should contain a dialog", () => {
-  //   expect(wrapper.get(".v-dialog__container").exists()).toBe(true);
-  // });
-  // it("should call GET_DATA action", () => {
-  //   expect(actions.GET_DATA).toHaveBeenCalled();
-  // });
+  it("should dispatch SIGN_UP action", () => {
+    const spy = jest.spyOn(actions, "SIGN_UP");
+    wrapper.vm.signUp();
+    expect(spy).toBeCalled();
+  });
+  it("should reject SIGN_UP action", () => {
+    wrapper.vm.$store._actions.SIGN_UP[0] = jest.fn(() => {
+      return Promise.reject();
+    });
+    wrapper.vm.signUp();
+  });
+  it("should dispatch LOGIN action", () => {
+    const spy = jest.spyOn(actions, "LOGIN");
+    wrapper.vm.logIn();
+    expect(spy).toBeCalled();
+  });
+  it("should reject LOGIN action", () => {
+    wrapper.vm.$store._actions.LOGIN[0] = jest.fn(() => {
+      return Promise.reject();
+    });
+    wrapper.vm.logIn();
+  });
+  it("should dispatch LOGOUT action", () => {
+    const spy = jest.spyOn(actions, "LOGOUT");
+    wrapper.vm.logOut();
+    expect(spy).toBeCalled();
+  });
+  it("should reject LOGOUT action", () => {
+    wrapper.vm.$store._actions.LOGOUT[0] = jest.fn(() => {
+      return Promise.reject();
+    });
+    wrapper.vm.logOut();
+  });
+  it("should reject GET_DATA action", () => {
+    actions = {
+      ...actions,
+      GET_DATA: jest.fn(() => {
+        return Promise.reject();
+      })
+    };
+    store = new Vuex.Store({
+      getters,
+      actions
+    });
+    wrapper = factory();
+    wrapper.vm.logOut();
+  });
 });
