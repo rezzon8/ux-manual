6fba56eca29f71a897360a802fb19b194b3bd4dd
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-13 12:02:37 +0200

    build dist

--- dist/precache-manifest.aef9253ac88ab5afb6eb434158c3bb29.js
+++ dist/precache-manifest.aef9253ac88ab5afb6eb434158c3bb29.js
@@ -1,14 +1,14 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "de4527c5851ea78fae1e",
+    "revision": "98801bdbbd933aede963",
     "url": "/css/app.e0c158b4.css"
   },
   {
-    "revision": "265be50272d97b1f4a2b",
+    "revision": "dd5a581a88ab335c71e7",
     "url": "/css/chunk-vendors.a0fe50b8.css"
   },
   {
-    "revision": "f7ef26a022b8d0491c0067526a42e7f7",
+    "revision": "585d5695448371524def70c1357a88d8",
     "url": "/index.html"
   },
   {
@@ -16,12 +16,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/js/about.bf972cba.js"
   },
   {
-    "revision": "de4527c5851ea78fae1e",
-    "url": "/js/app.5c716cc7.js"
+    "revision": "98801bdbbd933aede963",
+    "url": "/js/app.62026b7b.js"
   },
   {
-    "revision": "265be50272d97b1f4a2b",
-    "url": "/js/chunk-vendors.3cd1357a.js"
+    "revision": "dd5a581a88ab335c71e7",
+    "url": "/js/chunk-vendors.4e8a9bcb.js"
   },
   {
     "revision": "424fe9f75e9ec109b64a22dbe1d9d680",
