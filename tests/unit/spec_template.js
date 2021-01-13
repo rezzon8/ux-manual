846ef962f1d4d6d836c3739feaf0510189034456
Clinton Botha <clinton@tangentsolutions.co.za>
2021-01-13 16:42:58 +0200

    docs: add spec template for spec boilerplate code

---
+++ tests/unit/spec_template.js
@@ -0,0 +1,93 @@
+import { createLocalVue, mount } from "@vue/test-utils";
+import Vue from "vue";
+import Vuex from "vuex";
+import Vuetify from "vuetify";
+import VueRouter from "vue-router";
+import Vuelidate from "vuelidate";
+import App from "@/App.vue";
+
+Vue.use(Vuelidate);
+Vue.use(Vuetify); // added to prevent vuetify lint errors
+
+const localVue = createLocalVue();
+const vuetify = new Vuetify();
+
+let wrapper;
+let router;
+let store;
+let state;
+let getters;
+let actions;
+let mutations;
+
+localVue.use(VueRouter);
+localVue.use(Vuex);
+
+router = new VueRouter();
+
+const factory = () => {
+  return mount(App, {
+    propsData: {},
+    data: () => ({
+      email: "test@test.com",
+      password: "12345678",
+      confirmPassword: "12345678",
+      showPassword: false
+    }),
+    mocks: {},
+    stubs: {},
+    store,
+    vuetify,
+    router,
+    localVue
+  });
+};
+
+describe("App.vue", () => {
+  beforeEach(() => {
+    state = {
+      key: {}
+    };
+    getters = {
+      userIsAuthenticated: jest.fn(false)
+    };
+    actions = {
+      GET_DATA: jest.fn(),
+      LOGIN: jest.fn(),
+      SIGN_UP: jest.fn(),
+      LOGOUT: jest.fn()
+    };
+    mutations = {
+      someMutation: jest.fn()
+    };
+    store = new Vuex.Store({
+      state,
+      getters,
+      actions,
+      mutations
+    });
+    wrapper = factory();
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+  it("should render", () => {
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("should define a signUp method", () => {
+    expect(wrapper.vm.signUp).toBeDefined();
+  });
+  it("should define a logIn method", () => {
+    expect(wrapper.vm.logIn).toBeDefined();
+  });
+  it("should define a logOut method", () => {
+    expect(wrapper.vm.logOut).toBeDefined();
+  });
+  it("should call logIn when the button is clicked", () => {
+    const spy = jest.spyOn(wrapper.vm.store.actions, "SIGN_UP");
+    wrapper.vm.signUp();
+    expect(spy).toBeCalled();
+  });
+});
